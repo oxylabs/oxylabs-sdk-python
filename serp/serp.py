1f6cd5f15ac3e6ae704450238082bf5049011aa0
Muhammad Hamdan <muhammad.hamdan@mslm.io>
2024-02-20 17:50:58 +0500

    add initial classes

---
+++ serp/serp.py
@@ -0,0 +1,25 @@
+from internal.internal import Client, ApiCredentials
+
+class SerpClient:
+    def __init__(self, username, password):
+        """
+        Initializes a synchronous SERP client.
+
+        Args:
+            username (str): The username for API authentication.
+            password (str): The password for API authentication.
+        """
+        api_credentials = ApiCredentials(username, password)
+        self.client = Client('SyncBaseUrl', api_credentials)
+
+class SerpClientAsync:
+    def __init__(self, username, password):
+        """
+        Initializes an asynchronous SERP client.
+
+        Args:
+            username (str): The username for API authentication.
+            password (str): The password for API authentication.
+        """
+        api_credentials = ApiCredentials(username, password)
+        self.client = Client('AsyncBaseUrl', api_credentials)
