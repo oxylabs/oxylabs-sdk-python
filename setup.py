f464f1ac7b8ebe3038ba0e4986aa68b57184543a
haris-mslm <haris.ahmed@mslm.io>
2024-03-18 00:24:09 +0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+from oxylabs.version import SDK_VERSION
+
+long_description = """
+The official Python library for Oxylabs.
+
+Collect public data at scale with industry-leading web scraping solutions and the world's largest ethical proxy network.
+Documentation can be found at [https://github.com/mslmio/oxylabs-sdk-python](https://github.com/mslmio/oxylabs-sdk-python).
+"""
+
+setup(
+    name="oxylabs",
+    version=SDK_VERSION,
+    description="Official Python library for Oxylabs",
+    long_description=long_description,
+    url="https://oxylabs.io/",
+    author="Oxylabs",
+    author_email="support@oxylabs.io",
+    license="MIT",
+    packages=["oxylabs"],
+    install_requires=["aiohttp", "requests"],
+)
