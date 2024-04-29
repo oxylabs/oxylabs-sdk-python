496f50e67d0156f03e92db24494f86e958fa8836
haris-mslm <haris.ahmed@mslm.io>
2024-04-29 16:42:42 +0500

    docstring fmt

--- src/oxylabs/proxy/proxy.py
+++ src/oxylabs/proxy/proxy.py
@@ -48,10 +48,12 @@ class Proxy:
 
         Args:
             url (str): The URL to send the GET request to.
-            request_timeout (Optional[int]): The request timeout in seconds. Defaults to None (no timeout).
+            request_timeout (Optional[int]): The request timeout in seconds. 
+            Defaults to None (no timeout).
 
         Returns:
-            Optional[requests.Response]: The response object returned by the GET request, or None if an error occurred.
+            Optional[requests.Response]: The response object returned by the 
+            GET request, or None if an error occurred.
         """
         try:
             config = prepare_config(request_timeout=request_timeout)
@@ -73,22 +75,33 @@ class Proxy:
     def add_user_agent_header(self, user_agent_type: str) -> None:
         """
         Adds a user agent header to the session headers.
-        There is no way to indicate a specific User-Agent, but you can let us know which user-agent type you would like us to use.
+        There is no way to indicate a specific User-Agent, but you can let us 
+        know which user-agent type you would like us to use.
 
         Args:
-            user_agent_type (str): The user agent to add. Must be one of the following:
+            user_agent_type (str): The user agent to add. Must be one of the 
+            following:
             - "desktop"         - A User-Agent of a desktop browser.
-            - "desktop_chrome"  - A User-Agent of one of the latest versions of a desktop Chrome browser.
-            - "desktop_edge"    - A User-Agent of one of the latest versions of a desktop Edge browser.
-            - "desktop_firefox" - A User-Agent of one of the latest versions of a desktop Firefox browser.
-            - "desktop_opera"   - A User-Agent of one of the latest versions of a desktop Opera browser.
-            - "desktop_safari"  - A User-Agent of one of the latest versions of a desktop Safari browser.
+            - "desktop_chrome"  - A User-Agent of one of the latest versions of
+              a desktop Chrome browser.
+            - "desktop_edge"    - A User-Agent of one of the latest versions of
+              a desktop Edge browser.
+            - "desktop_firefox" - A User-Agent of one of the latest versions of
+              a desktop Firefox browser.
+            - "desktop_opera"   - A User-Agent of one of the latest versions of
+              a desktop Opera browser.
+            - "desktop_safari"  - A User-Agent of one of the latest versions of
+              a desktop Safari browser.
             - "mobile"          - A User-Agent of a mobile browser.
-            - "mobile_android"  - A User-Agent of one of the latest versions of an Android mobile browser.
-            - "mobile_ios"      - A User-Agent of one of the latest versions of an iOS mobile browser.
+            - "mobile_android"  - A User-Agent of one of the latest versions of
+              an Android mobile browser.
+            - "mobile_ios"      - A User-Agent of one of the latest versions of
+              an iOS mobile browser.
             - "tablet"          - A User-Agent of a tablet browser.
-            - "tablet_android"  - A User-Agent of one of the latest versions of an Android tablet browser.
-            - "tablet_ios"      - A User-Agent of one of the latest versions of an iOS tablet browser.
+            - "tablet_android"  - A User-Agent of one of the latest versions of
+              an Android tablet browser.
+            - "tablet_ios"      - A User-Agent of one of the latest versions of
+              an iOS tablet browser.
 
         Returns:
             None
@@ -116,8 +129,10 @@ class Proxy:
         Adds a parse header to the session headers.
 
         Args:
-            parse (bool, optional): Whether to enable parsing. Defaults to False.
-            parsing_instructions (dict, optional): Instructions for parsing. Defaults to None.
+            parse (bool, optional): Whether to enable parsing. Defaults to 
+            False.
+            parsing_instructions (dict, optional): Instructions for parsing. 
+            Defaults to None.
 
         Returns:
             None
@@ -139,7 +154,8 @@ class Proxy:
         Checks if the URL to scrape belongs to a universal source.
 
         Returns:
-            bool: True if the URL belongs to a universal source, False otherwise.
+            bool: True if the URL belongs to a universal source, False 
+            otherwise.
         """
         parsed_url = urlparse(self._url_to_scrape)
         if any(
@@ -153,10 +169,12 @@ class Proxy:
     def add_geo_location_header(self, geo_location: str) -> None:
         """
         Adds a geo location header to the session headers.
-        In some cases, you may need to indicate the geographical location that the result should be adapted for.
+        In some cases, you may need to indicate the geographical location that 
+        the result should be adapted for.
 
         Args:
-            geo_location (str): The geo location to add. Accepted values depend on the URL you would like us to scrape.
+            geo_location (str): The geo location to add. Accepted values depend 
+            on the URL you would like us to scrape.
 
         Returns:
             None
